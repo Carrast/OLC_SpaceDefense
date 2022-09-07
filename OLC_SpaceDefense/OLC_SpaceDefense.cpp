4c1b1d8ab66308dae41ea7c2b609fa38a361f5ba
Csanyi Bence <csanyibence@gmail.com>
2022-09-07 20:35:17 +0200

    Add project files.

---
+++ OLC_SpaceDefense/OLC_SpaceDefense.cpp
@@ -0,0 +1,323 @@
+// OLC_SpaceDefense.cpp
+// written by Carrast
+// 2022.09.07
+// all rights reserved
+// thanks javidx9!
+
+#include <iostream>
+#include <string>
+#include <math.h>
+#include "olcConsoleGameEngine.h"
+
+class SpaceDefense : public olcConsoleGameEngine
+{
+public:
+    SpaceDefense();
+    ~SpaceDefense() {}
+
+private:
+    struct sStars
+    {
+        float x;
+        float y;
+        float dx;
+        float dy;
+    };
+
+    struct sBullets
+    {
+        float x;
+        float y;
+        float dx;
+        float dy;
+        int damage;
+    };
+
+    struct sObjects
+    {
+        float x;
+        float y;
+        float dx;
+        float dy;
+        float angle;
+        float scale;
+        int health;
+    };
+
+    std::vector<sStars> vecStars;
+    sObjects tank;
+    std::vector<sBullets> vecBullets;
+    std::vector<sObjects> vecMeteors;
+    std::vector<sObjects> vecDebris;
+
+    std::vector<std::pair<float, float>>  vecModelTank;
+    std::vector<std::pair<float, float>>  vecModelMeteors;
+    std::vector<std::pair<float, float>>  vecModelDebris;
+
+    float speedBoost = 0.0f;
+    int groundLevel = 95;
+    int nScore;
+    bool bDead;
+
+protected:
+    // Called by olcConsoleGameEngine
+    virtual bool OnUserCreate()
+    {
+        // background stars
+        vecStars.push_back({ 32.0f, 5.0f, -4.5f, 0.0f });
+        vecStars.push_back({ 2.0f, 10.0f, -4.5f, 0.0f });
+        vecStars.push_back({ 150.0f, 12.0f, -4.5f, 0.0f });
+        vecStars.push_back({ 22.0f, 23.0f, -4.5f, 0.0f });
+        vecStars.push_back({ 84.0f, 32.0f, -4.5f, 0.0f });
+        vecStars.push_back({ 42.0f, 45.0f, -4.5f, 0.0f });
+        vecStars.push_back({ 131.0f, 49.0f, -4.5f, 0.0f });
+        vecStars.push_back({ 30.0f, 56.0f, -4.5f, 0.0f });
+        vecStars.push_back({ 122.0f, 87.0f, -4.5f, 0.0f });
+        vecStars.push_back({ 15.0f, 92.0f, -4.5f, 0.0f });
+        vecStars.push_back({ 62.0f, 70.0f, -4.5f, 0.0f });
+
+        // tank model
+        vecModelTank =
+        {
+            { 0.0f, -5.0f},
+            { -2.5f, 0.0f},
+            { 2.5f, 0.0f}
+        };
+
+        // initiate tank position
+        tank.x = ScreenWidth() / 2;
+        tank.y = groundLevel;
+        tank.dx = 0;
+        tank.dy = 0;
+        tank.angle = 0;
+        tank.scale = 1.0f;
+        tank.health = 100;
+
+        // initiate 3 meteors at game start
+        for (int i = 0; i < 3; i++)
+        {        
+            float rx = ((float)std::rand() / (float)RAND_MAX) * 6.0f - 3.0f;
+            float ry = ((float)std::rand() / (float)RAND_MAX) * 2.0f + 5.0f;
+            vecMeteors.push_back({38.f + (i * 30), -5.0f, rx, ry, 0.0f, 9.0f, 3});
+        }
+
+        // meteor model
+        int numSections = 20;
+        for (int i = 0; i < numSections; i++)
+        {
+            float radius = ((float)std::rand() / (float)RAND_MAX) * 0.4f + 0.7f;
+            float angle = ((float)i / (float)numSections) * 2.0f * 3.14159f;
+            vecModelMeteors.push_back(std::make_pair(radius * cosf(angle), radius * sinf(angle)));
+        }
+
+        // debris model
+        numSections = 5;
+        for (int i = 0; i < numSections; i++)
+        {
+            float radius = ((float)std::rand() / (float)RAND_MAX) * 0.3f + 0.2f;
+            float angle = ((float)i / (float)numSections) * 2.0f * 3.14159f;
+            vecModelDebris.push_back(std::make_pair(radius * cosf(angle), radius * sinf(angle)));
+        }
+
+        return true;
+    }
+
+    // Called by olcConsoleGameEngine
+    virtual bool OnUserUpdate(float fElapsedTime)
+    {
+        // clear screen, background fill, reset every frame
+        Fill(0, 0, m_nScreenWidth, 97, PIXEL_SOLID, 0);
+
+        // background stars
+        for (auto& s : vecStars)
+        {
+            s.x += s.dx * fElapsedTime;
+            s.y += s.dy * fElapsedTime;
+            WrapCoordinates(s.x, s.y, s.x, s.y);
+            Draw(s.x, s.y, PIXEL_SOLID, FG_DARK_GREY);
+        }
+
+        // tank movement
+        if (m_keys[VK_LSHIFT].bHeld)
+            speedBoost = 30.0f;
+        else
+            speedBoost = 0.0f;
+        if (m_keys[VK_LEFT].bHeld)
+            tank.x += -(35.0f + speedBoost) * fElapsedTime;
+        if (m_keys[VK_RIGHT].bHeld)
+            tank.x += +(35.0f + speedBoost) * fElapsedTime;
+
+        // limit tank mobility
+        if (tank.x < 3.5)
+            tank.x = 3.5;
+        if (tank.x > ScreenWidth() - 3.5)
+            tank.x = ScreenWidth() - 3.5;
+
+        // fire bullets
+        if (m_keys[VK_SPACE].bPressed)
+        {
+            vecBullets.push_back({ tank.x, tank.y - 5.0f, 0.0f, -55.0f, 5 });
+        }
+
+        // update and draw bullets
+        for (auto& b : vecBullets)
+        {
+            b.x += b.dx * fElapsedTime;
+            b.y += b.dy * fElapsedTime;
+            Draw(b.x, b.y, PIXEL_SOLID, FG_RED);
+        }
+
+        // check collision with meteors
+        for (auto& b : vecBullets)
+        {
+            for (auto& m : vecMeteors)
+            {
+                if (IsPointInCircle(m.x, m.y, m.scale, b.x, b.y))
+                {
+                    // meteor hit! remove from gamespace
+                    b.y = -100;
+                    nScore += 100;
+
+                    // create 2 debris falling from the sky
+                    float nx1 = ((float)std::rand() / (float)RAND_MAX) * 10.0f - 5.0f;
+                    float ny1 = ((float)std::rand() / (float)RAND_MAX) * 10.0f - 5.0f;
+                    float nx2 = ((float)std::rand() / (float)RAND_MAX) * 10.0f - 5.0f;
+                    float ny2 = ((float)std::rand() / (float)RAND_MAX) * 10.0f - 5.0f;
+                    vecDebris.push_back({m.x, m.y, nx1, ny1, 0.0f, 7.0f, 1});
+                    vecDebris.push_back({m.x, m.y, nx2, ny2, 0.0f, 7.0f, 1});
+
+                    // remove meteor from gamespace
+                    m.x = -100;
+                }
+            }
+        }
+
+        // TODO
+        // update and draw debris
+        for (auto& d : vecDebris)
+        {
+            d.x += d.dx * fElapsedTime; //std::sqrtf(d.x) * fElapsedTime;
+            d.y += d.dy * fElapsedTime;
+            d.angle += (((float)std::rand() / (float)RAND_MAX) * 6.0f - 3.0f) * fElapsedTime;
+            DrawWireFrameModel(vecModelDebris, d.x, d.y, d.angle, d.scale, PIXEL_SOLID, FG_YELLOW);
+        }
+
+        // update and draw meteors
+        for (auto& m : vecMeteors)
+        {
+            m.x += m.dx * fElapsedTime;
+            m.y += m.dy * fElapsedTime;
+            m.angle += (((float)std::rand() / (float)RAND_MAX) * 6.0f - 3.0f) * fElapsedTime;
+            DrawWireFrameModel(vecModelMeteors, m.x, m.y, m.angle, m.scale, PIXEL_SOLID, FG_YELLOW);
+        }
+
+        // remove off screen bullets
+        if (vecBullets.size() > 0)
+        {
+            auto i = std::remove_if(vecBullets.begin(), vecBullets.end(), [&](sBullets b) {return (b.y < 0); });
+            if (i != vecBullets.end())
+            {
+                vecBullets.erase(i);
+            }
+        }
+
+        // remove off screen meteors
+        if (vecMeteors.size() > 0)
+        {
+            auto i = std::remove_if(vecMeteors.begin(), vecMeteors.end(), [&](sObjects m) { return (m.x < -13) || m.x >(ScreenWidth() + 13); });
+            if (i != vecMeteors.end())
+            {
+                vecMeteors.erase(i);
+            }
+        }
+
+        // draw tank
+        DrawWireFrameModel(vecModelTank, tank.x, tank.y, tank.angle, tank.scale, PIXEL_SOLID, FG_WHITE);
+
+        // monitoring
+        DrawString(2, 2, L"nBullets: " + std::to_wstring(vecBullets.size()));
+        DrawString(60, 2, L"nScore: " + std::to_wstring(nScore));
+
+        // draw ground
+        Fill(0, 97, m_nScreenWidth, m_nScreenHeight, PIXEL_SOLID, FG_GREY);
+
+        return true;
+    }
+
+// --- special functions ---
+protected:
+    bool IsPointInCircle(float cx, float cy, float r, float x, float y)
+    {
+        float distance = std::sqrtf( (cx - x) * (cx - x) + (cy - y) * (cy - y));
+        if (distance <= r)
+            return true;
+        else
+            return false;
+    }
+    
+    void WrapCoordinates(float ix, float iy, float& ox, float& oy)
+    {
+        ox = ix;
+        oy = iy;
+        if (ix < 0)
+            ox = ix + ScreenWidth();
+        if (ix > ScreenWidth())
+            ox = ix - ScreenWidth();
+        if (iy < 0)
+            oy = iy + ScreenHeight();
+        if (iy > ScreenHeight())
+            oy = iy - ScreenWidth();
+    }
+
+    void DrawWireFrameModel(const std::vector<std::pair<float, float>> &vecModelCoordinates, float x, float y, float a, float s, short c, short col)
+    {
+        // first = x
+        // second = y
+        
+        std::vector<std::pair<float, float>> TransformedCoordinates;
+        int verts = vecModelCoordinates.size();
+        TransformedCoordinates.resize(verts);
+
+        // rotate
+        for (int i = 0; i < verts; i++)
+        {
+            TransformedCoordinates[i].first = vecModelCoordinates[i].first * cosf(a) - vecModelCoordinates[i].second * sinf(a);
+            TransformedCoordinates[i].second = vecModelCoordinates[i].first * sinf(a) + vecModelCoordinates[i].second * cosf(a);
+        }
+
+        // scale
+        for (int i = 0; i < verts; i++)
+        {
+            TransformedCoordinates[i].first = TransformedCoordinates[i].first * s;
+            TransformedCoordinates[i].second = TransformedCoordinates[i].second * s;
+        }
+
+        // translate
+        for (int i = 0; i < verts; i++)
+        {
+            TransformedCoordinates[i].first = TransformedCoordinates[i].first + x;
+            TransformedCoordinates[i].second = TransformedCoordinates[i].second + y;
+        }
+
+        // draw
+        for (int i = 0; i < verts + 1; i++)
+        {
+            int j = i + 1;
+            DrawLine(TransformedCoordinates[i % verts].first, TransformedCoordinates[i % verts].second, TransformedCoordinates[j % verts].first, TransformedCoordinates[j % verts].second, c, col);
+        }
+    }
+};
+
+SpaceDefense::SpaceDefense()
+{
+    m_sAppName = L"Space Defense";
+}
+
+int main()
+{
+    SpaceDefense game;
+    game.ConstructConsole(160, 100, 8, 8);
+    game.Start();
+    return 0;
+}
+
